2bf01a60b0985e503becbe072e6e42e2a3794f39
LucasCunhaAlmeida <lucascunhaalmeida2003@gmail.com>
2024-05-15 20:46:43 -0300

    Métodos para armazenar os números
    Foi adicionado os métodos para armazenar os números que o usuário escolhe. Assim pode se dar continuidade a outros métodos.

--- src/Processador.java
+++ src/Processador.java
@@ -7,15 +7,11 @@ public class Processador extends PainelGrafico{
     public Processador () {
         instanciaAcaoBotoes ();
     }
-    static double numero_pressionado = 0;
+    static Integer numero_pressionado = 0;
     static double num1 = 0;
     static double num2 = 0;
-    char operacao_pressionado;
-    public void somar() {
+    static char operacao_pressionado;
 
-        if (num1 != 0 && num2 != 0)
-            System.out.println(" " + (num1 + num2));
-    }
 
     public void lerOpcaoBotao(ActionEvent e) {
 
@@ -23,108 +19,204 @@ public class Processador extends PainelGrafico{
 
         if (botaoPressionado == botao_0) {
             numero_pressionado = 0;
-            obterNum1eNum2(false);
+            controleNumeros(false);
         } else if (botaoPressionado == botao_1) {
             numero_pressionado = 1;
-            obterNum1eNum2(false);
+            controleNumeros(false);
         } else if (botaoPressionado == botao_2) {
             numero_pressionado = 2;
-            obterNum1eNum2(false);
+            controleNumeros(false);
         } else if (botaoPressionado == botao_3) {
             numero_pressionado = 3;
-            obterNum1eNum2(false);
+            controleNumeros(false);
         } else if (botaoPressionado == botao_4) {
             numero_pressionado = 4;
-            obterNum1eNum2(false);
+            controleNumeros(false);
         } else if (botaoPressionado == botao_5) {
             numero_pressionado = 5;
-            obterNum1eNum2(false);
+            controleNumeros(false);
         } else if (botaoPressionado == botao_6) {
             numero_pressionado = 6;
-            obterNum1eNum2(false);
+            controleNumeros(false);
         } else if (botaoPressionado == botao_7) {
             numero_pressionado = 7;
-            obterNum1eNum2(false);
+            controleNumeros(false);
         } else if (botaoPressionado == botao_8) {
             numero_pressionado = 8;
-            obterNum1eNum2(false);
+            controleNumeros(false);
         } else if (botaoPressionado == botao_9) {
             numero_pressionado = 9;
-            obterNum1eNum2(false);
+            controleNumeros(false);
         } else if (botaoPressionado == botao_igual) {
             operacao_pressionado = '=';
-            obterNum1eNum2(true);
+            controleNumeros(true);
         } else if (botaoPressionado == botao_soma) {
             operacao_pressionado = '+';
-            obterNum1eNum2(true);
+            controleNumeros(true);
         } else if (botaoPressionado == botao_subt) {
             operacao_pressionado = '-';
-            obterNum1eNum2(true);
+            controleNumeros(true);
         } else if (botaoPressionado == botao_mult) {
             operacao_pressionado = 'x';
-            obterNum1eNum2(true);
+            controleNumeros(true);
         } else if (botaoPressionado == botao_div) {
             operacao_pressionado = '/';
-            obterNum1eNum2(true);
+            controleNumeros(true);
         } else if (botaoPressionado == botao_porcen) {
             operacao_pressionado = '%';
-            obterNum1eNum2(true);
+            controleNumeros(true);
         } else if (botaoPressionado == botao_virg) {
             operacao_pressionado = ',';
-            obterNum1eNum2(true);
+            controleNumeros(false);
         } else {
             operacao_pressionado = 'A'; // Abreviação de AC.
-            obterNum1eNum2(true);
+            controleNumeros(true);
         }
 
     }
 
-    /* Quando estiver com zero, se pega o número e coloca no num1
-       Quando estiver com um, se pega o número e coloca no num2. */
-    static int pegar_num1_ou_num2 = 0;
+    static int num1_ou_num2 = 1;
+    public static void controleNumeros(boolean operador) {
 
-    public static void obterNum1eNum2(boolean e_operador) {
+        // Escolhendo qual variavel vai ser preenchida, 1 para num1 e 2 para num2.
+        if (num1_ou_num2 == 1) {
+            // A vez aqui é do num1.
 
-        if (!e_operador) {
+            if (!operador) {
+                // Se for não for algum dos operadores, mas pode ser uma vírgula.
 
-            if (pegar_num1_ou_num2 == 0) {
-                // concatenanado os números.
+                // Chamando a o método para preencher o num1
+                obterNum1(false);
 
-                String str_num1 = Double.toString(numero_pressionado).replaceAll("\\.", "");
-                String str_num2 = Double.toString(num1).replaceAll("\\.", "");
+            } else {
+                /* Mandamos um false para obterNum1, pois esse é o sinal para
+                   concatenar os números guardados antes e após a vírgula,
+                   fazendo com que se preencha o num1 com o número completo. */
 
-                // Convertendo os dois.
-                num1 = Double.parseDouble(str_num2 + str_num1);
-            } else if (pegar_num1_ou_num2 == 1) {
-                // concatenanado os números.
+                obterNum1(true);
 
-                // Transformando o número pressionado em String.
-                String str_num1 = Double.toString(numero_pressionado).replaceAll("\\.", "");
-                String str_num2 = Double.toString(num1).replaceAll("\\.", "");
+                // Agora trocamos de num1 para num2, a variável a ser preenchida.
+                num1_ou_num2 = 2;
 
-                num2 = Double.parseDouble(str_num2 + str_num1);
             }
+        } else if (num1_ou_num2 == 2) {
+            // A vez aqui é do num2.
 
-        } else if (e_operador) {
+            if (!operador) {
+                // Se for não for algum dos operadores, mas pode ser uma vírgula.
 
-            if (pegar_num1_ou_num2 == 0) {
-                pegar_num1_ou_num2 = 1;
-            } else if (pegar_num1_ou_num2 == 1) {
+                // Chamando a o método para preencher o num2
+                obterNum2(false);
+            } else {
+                /* Mandamos um false para obterNum2, pois esse é o sinal para
+                   concatenar os números guardados antes e após a vírgula,
+                   fazendo com que se preencha o num2 com o número completo. */
 
-                /* Chamar o método para fazer a operação de acordo com o operador,
-                   pois se chegar a trocar o número que quero preencher depois de
-                   pegar algum operador, é por conta que a pessoa quer fazer alguma
-                   operação matemática. (após a operação zerar o num1 e num2) */
+                obterNum2(true);
 
-                System.out.println("num1: " + num1);
-                System.out.println("num2: " + num2);
-                System.out.println("num1 + num2 = " + (num1+num2));
-                pegar_num1_ou_num2 = 0;
+                // Agora trocamos de num2 para num1, a variável a ser preenchida.
+                num1_ou_num2 = 1;
+
             }
 
         }
 
     }
+
+    
+    // Variáveis usadas para armazenar os números após a vírgula e antes
+    static Integer num_temp = 0, num_virg_temp = 0;
+    public static void obterNum1(boolean operador) {
+
+        if (operacao_pressionado != ',') {
+            // Se não foi digitado uma vírgula, então pegaremos a parte inteira.
+
+            num_temp = num_temp * 10 + numero_pressionado;
+            numero_pressionado = 0;
+            System.out.println("num_temp = " + num_temp);
+
+        } else {
+            /* Se pressionou para adicionar virgula, precisamos pegar os
+               números após a virgula. */
+
+            num_virg_temp = num_virg_temp * 10 + numero_pressionado;
+            numero_pressionado = 0;
+            System.out.println("num_virg_temp = " + num_virg_temp);
+
+        }
+
+        // Agora testando se foi passado um operador em vez de um número.
+        if (operador) {
+
+            /* Se chegou aqui é por conta que foi pressionado algum operador,
+               então temos que converter os números após a virgula e antes da vírgula,
+               para se colocar eles na variável num1. */
+
+            // Obtendo o número de dígitos na parte decimal
+            int numDigitosDecimal = String.valueOf(num_virg_temp).length();
+
+            // Calculando o divisor
+            int divisor = (int) Math.pow(10, numDigitosDecimal);
+
+            // Combinando a parte inteira e a parte decimal
+
+            num1 = (num_temp.doubleValue()/10) + ((double) num_virg_temp / divisor);
+
+            // limpando as variáveis que usamos para armazenar números temporários.
+            num_virg_temp = 0;
+            num_temp = 0;
+
+            System.out.println("num1 após tudo = " + num1);
+        }
+
+    }
+
+
+    public static void obterNum2(boolean operador) {
+
+        if (operacao_pressionado != ',') {
+            // Se não foi digitado uma vírgula, então pegaremos a parte inteira.
+
+            num_temp = num_temp * 10 + numero_pressionado;
+            numero_pressionado = 0;
+            System.out.println("num_temp = " + num_temp);
+
+        } else {
+            /* Se pressionou para adicionar virgula, precisamos pegar os
+               números após a virgula. */
+
+            num_virg_temp = num_virg_temp * 10 + numero_pressionado;
+            numero_pressionado = 0;
+            System.out.println("num_virg_temp = " + num_virg_temp);
+
+        }
+
+        // Agora testando se foi passado um operador em vez de um número.
+        if (operador) {
+
+            /* Se chegou aqui é por conta que foi pressionado algum operador,
+               então temos que converter os números após a virgula e antes da vírgula,
+               para se colocar eles na variável num1. */
+
+            // Obtendo o número de dígitos na parte decimal
+            int numDigitosDecimal = String.valueOf(num_virg_temp).length();
+
+            // Calculando o divisor
+            int divisor = (int) Math.pow(10, numDigitosDecimal);
+
+            // Combinando a parte inteira e a parte decimal
+
+            num2 = (num_temp.doubleValue()/10) + ((double) num_virg_temp / divisor);
+
+            // limpando as variáveis que usamos para armazenar números temporários.
+            num_virg_temp = 0;
+            num_temp = 0;
+
+            System.out.println("num2 após tudo = " + num2);
+        }
+    }
+
+
     public void instanciaAcaoBotoes () {
 
         botao_igual.addActionListener(this::lerOpcaoBotao);
